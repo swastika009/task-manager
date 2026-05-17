748254d2f633a1b094615b546b6860caa95a8c1b
swastika009 <152474022+swastika009@users.noreply.github.com>
2026-05-17 14:26:29 +0530

    fix: support DATABASE_URL on production

--- server/db/index.js
+++ server/db/index.js
@@ -1,13 +1,20 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
-const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: 'postgres',
-  ssl: { rejectUnauthorized: false }
-});
+const pool = new Pool(
+  process.env.DATABASE_URL
+    ? {
+        connectionString: process.env.DATABASE_URL,
+        ssl: { rejectUnauthorized: false }
+      }
+    : {
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: 'postgres',
+        ssl: { rejectUnauthorized: false }
+      }
+);
 
 module.exports = pool;
